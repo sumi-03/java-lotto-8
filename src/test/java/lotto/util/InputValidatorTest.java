e067bef705710992382d145c23b67a0b3c82ae91
sumi <lsmkoo03@naver.com>
2025-11-03 15:39:46 +0900

    test: InputValidator 테스트를 Given–When–Then 구조로 통일, 공통 당첨 번호 상수화

--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -9,116 +9,181 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class InputValidatorTest {
+    private static final List<Integer> VALID_WINNING_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
 
     @Test
     @DisplayName("금액이 1000원 단위의 양수이면 예외가 발생하지 않는다")
     void 유효한_구입금액() {
-        assertDoesNotThrow(() -> InputValidator.validatePurchaseAmount("8000"));
+        // given
+        String input = "8000";
+
+        // when & then
+        assertDoesNotThrow(() -> InputValidator.validatePurchaseAmount(input));
     }
 
     @Test
     @DisplayName("금액이 숫자가 아니면 예외가 발생한다")
     void 숫자가_아닌_입력_예외() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("abc"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+        // given
+        String input = "abc";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("금액이 음수이면 예외가 발생한다")
     void 음수_입력_예외() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("-1000"))
+        // given
+        String input = "-1000";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("금액이 0이면 예외가 발생한다")
     void 금액이_0이면_예외() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("0"))
+        // given
+        String input = "0";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("금액이 1000원 단위가 아니면 예외가 발생한다")
     void 천원단위_아니면_예외() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("1500"))
+        // given
+        String input = "1500";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호가 1~45 범위의 6개 숫자면 예외가 발생하지 않는다")
     void 유효한_당첨번호() {
-        assertThatCode(() -> InputValidator.validateWinningNumbers("1,2,3,4,5,6"))
+        // given
+        String input = "1,2,3,4,5,6";
+
+        // when & then
+        assertThatCode(() -> InputValidator.validateWinningNumbers(input))
                 .doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("당첨 번호가 6개 미만이면 예외가 발생한다")
     void 당첨번호_부족_예외() {
-        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,4,5"))
+        // given
+        String input = "1,2,3,4,5";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호가 6개 초과면 예외가 발생한다")
     void 당첨번호_초과_예외() {
-        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,4,5,6,7"))
+        // given
+        String input = "1,2,3,4,5,6,7";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다")
     void 당첨번호_중복_예외() {
-        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,3,4,5"))
+        // given
+        String input = "1,2,3,3,4,5";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호가 숫자가 아니면 예외가 발생한다")
     void 당첨번호_숫자아님_예외() {
-        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,세,4,5,6"))
+        // given
+        String input = "1,2,세,4,5,6";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("당첨 번호가 1~45 범위를 벗어나면 예외가 발생한다")
     void 당첨번호_범위초과_예외() {
-        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,4,5,46"))
+        // given
+        String input = "1,2,3,4,5,46";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다")
     void 보너스번호_숫자아님_예외() {
-        assertThatThrownBy(() -> InputValidator.validateBonusNumber("abc", List.of(1, 2, 3, 4, 5, 6)))
+        // given
+        String input = "abc";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input, VALID_WINNING_NUMBERS))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호가 1 미만이면 예외가 발생한다")
     void 보너스번호_0이하_예외() {
-        assertThatThrownBy(() -> InputValidator.validateBonusNumber("0", List.of(1, 2, 3, 4, 5, 6)))
+        // given
+        String input = "0";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input, VALID_WINNING_NUMBERS))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호가 45 초과이면 예외가 발생한다")
     void 보너스번호_45초과_예외() {
-        assertThatThrownBy(() -> InputValidator.validateBonusNumber("46", List.of(1, 2, 3, 4, 5, 6)))
+        // given
+        String input = "46";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input, VALID_WINNING_NUMBERS))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
 
     @Test
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다")
     void 보너스번호_중복_예외() {
-        assertThatThrownBy(() -> InputValidator.validateBonusNumber("3", List.of(1, 2, 3, 4, 5, 6)))
+        // given
+        String input = "3";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input, VALID_WINNING_NUMBERS))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("보너스 번호가 정상 범위이고 중복되지 않으면 예외가 발생하지 않는다")
     void 유효한_보너스번호() {
-        assertThatCode(() -> InputValidator.validateBonusNumber("7", List.of(1, 2, 3, 4, 5, 6)))
+        // given
+        String input = "7";
+
+        // when & then
+        assertThatCode(() -> InputValidator.validateBonusNumber(input, VALID_WINNING_NUMBERS))
                 .doesNotThrowAnyException();
     }
 }
